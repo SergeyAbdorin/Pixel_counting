552d32eb9a990df8615cbc4bc688cefb8c9f1e37
SergeyA <sergey.abdorin@gmail.com>
2021-09-18 01:13:03 +0700

    fix urlpatterns for MEDIA_ROOT

--- pixel/pixel/urls.py
+++ pixel/pixel/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import handler404, handler500
+from django.views.static import serve
 from django.urls import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 handler404 = "backend.views.page_not_found"
 handler500 = "backend.views.server_error"
@@ -11,15 +12,16 @@ handler500 = "backend.views.server_error"
 urlpatterns = [
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-    path("", include("backend.urls"))
+    path("", include("backend.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
+urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT
+
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}
     )
+]
